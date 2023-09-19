d7ac5165a3493f00cdff02c56b7cc73972ace40f
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-09-19 14:24:51 -0700

    tmp: attempting to get ipfs node running

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ var assets embed.FS
 func main() {
 	// Create an instance of the app structure
 	app := NewApp()
+	// StartIpfs() // even commenting this out, getting errors from ipfs.go:
+	// ../../../../../.asdf/installs/golang/1.21.1/packages/pkg/mod/github.com/ipfs/kubo@v0.22.0/thirdparty/verifbs/verifbs.go:17:33: not enough arguments in call to verifcid.ValidateCid
+	// ../../../../../.asdf/installs/golang/1.21.1/packages/pkg/mod/github.com/libp2p/go-libp2p@v0.29.0/p2p/transport/quicreuse/quic_multiaddr.go:38:19: undefined: quic.VersionDraft29
 
 	// Create application with options
 	err := wails.Run(&options.App{
